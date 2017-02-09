247750187f50ee902e0d3b537d1897a0a101c1f8
marc wong <marcw@uci.edu>
2017-02-09 01:50:53 -0800

    contacts

--- js/algolia.js
+++ js/algolia.js
@@ -1,9 +1,12 @@
 var client = algoliasearch('A2CZQP61F0', '3898a6da222fc6822b20ae4cfb944087');
 var index = client.initIndex('quotes');
-var quotesJSON = json;
+// var quotesJSON = json;
 
-index.addObjects(quotesJSON, function(err, content) {
-  if(err){
-    console.error(err);
-  }
+// index.addObjects(quotesJSON, function(err, content) {
+//   if(err){
+//     console.error(err);
+//   }
+// });
+index.search('rocco', function(err, content) {
+  console.log(content.hits);
 });
