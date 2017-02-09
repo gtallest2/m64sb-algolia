20ba39fe28a74e40618b9205ef9fa6eb136e3870
marc wong <marcw@uci.edu>
2017-02-09 01:24:57 -0800

    https

--- js/algolia.js
+++ js/algolia.js
@@ -1,6 +1,4 @@
-var client = algoliasearch('A2CZQP61F0', '78429c5ee9cb638cdfc5bac29920f907', {
-  protocol: 'http:'
-});
+var client = algoliasearch('A2CZQP61F0', '78429c5ee9cb638cdfc5bac29920f907');
 var index = client.initIndex('quotes');
 var quotesJSON = json;
 
