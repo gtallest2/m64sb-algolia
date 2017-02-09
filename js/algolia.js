ceae00101ffb2e19642c2fdfe89429df7f9b15ae
marc wong <marcw@uci.edu>
2017-02-09 01:38:28 -0800

    contacts

--- js/algolia.js
+++ js/algolia.js
@@ -1,4 +1,4 @@
-var client = algoliasearch('A2CZQP61F0', '78429c5ee9cb638cdfc5bac29920f907');
+var client = algoliasearch('A2CZQP61F0', '3898a6da222fc6822b20ae4cfb944087');
 var index = client.initIndex('quotes');
 var quotesJSON = json;
 
